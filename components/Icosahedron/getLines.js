be9a8bbc4fccd85caaef6f0e3d50977263764415
peach-studio <nick.amao@foxmail.com>
2018-12-16 01:59:25 +0800

    esline fix

--- components/Icosahedron/getLines.js
+++ components/Icosahedron/getLines.js
@@ -1,25 +1,24 @@
-function getLines(points, xzChunk, xyChunk) {
-  let lines = [];
+function getLines (points, xzChunk, xyChunk) {
+  let lines = []
 
   for (let i = 0; i < xyChunk; i++) {
     for (let j = 0; j < xzChunk - 1; j++) {
-      lines.push([points[j + i * xzChunk], points[j + 1 + i * xzChunk]]);
+      lines.push([points[j + i * xzChunk], points[j + 1 + i * xzChunk]])
     }
 
-    lines.push([points[i * xzChunk], points[i * xzChunk + xzChunk - 1]]);
+    lines.push([points[i * xzChunk], points[i * xzChunk + xzChunk - 1]])
   }
 
   for (let i = 0; i < xzChunk; i++) {
     for (let j = 0; j < xyChunk - 1; j++) {
-      let startPointIndex = j * xzChunk + i;
-      let endPointIndex = (j + 1) * xzChunk + i;
+      let startPointIndex = j * xzChunk + i
+      let endPointIndex = (j + 1) * xzChunk + i
 
-      lines.push([points[startPointIndex], points[endPointIndex]]);
+      lines.push([points[startPointIndex], points[endPointIndex]])
     }
   }
 
-  return lines;
+  return lines
 }
 
-
-export default getLines;
+export default getLines
